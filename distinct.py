d511e407266d80151c9ec7b358580269c0898a76
khanz00024 <saafkh24@gmail.com>
2025-01-26 22:54:01 -0500

    Create distinct.py

---
+++ distinct.py
@@ -0,0 +1,15 @@
+def isDistinct(year: int) -> bool:
+    s = str(year)
+    digits_used = []
+    for char in s:
+        if char in digits_used:
+            return False
+        digits_used.append(char)
+    return True
+
+year = int(input())
+year = year + 1
+
+while not isDistinct(year):
+    year = year + 1
+print(year)
